c9ca2800ce817083e6b75c17d2bbafaada9083d0
bangef <ficrihnp@gmail.com>
2023-10-22 02:32:59 +0700

    fix: again

--- src/js/main2.js
+++ src/js/main2.js
@@ -4,34 +4,42 @@ import * as THREEx from "@ar-js-org/ar.js/three.js/build/ar-threex-location-only
 
 function main() {
 	const CANVAS = document.getElementById("canvas");
-	const SCENE = new THREE.Scene();
-	const CAMERA = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
 	const RENDERER = new THREE.WebGLRenderer({ canvas: CANVAS, antialias: true });
-	RENDERER.shadowMap.enabled = true;
+
+	const FOV = 60;
+	const ASPECT = 1.33;
+	const NEAR = 0.1;
+	const FAR = 100000;
+	const CAMERA = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
+	const SCENE = new THREE.Scene();
 	RENDERER.setPixelRatio(Math.min(2, window.devicePixelRatio));
-	// const AMBIENT = new THREE.AmbientLight(0x404040, 7.5);
+	const AMBIENT = new THREE.AmbientLight(0x404040, 7.5);
 	const ARJS = new THREEx.LocationBased(SCENE, CAMERA);
 	const CAM = new THREEx.WebcamRenderer(RENDERER);
+	const DOC = new THREEx.DeviceOrientationControls(CAMERA);
 
 	if (navigator.geolocation) {
 		navigator.geolocation.watchPosition((success) => console.log(success));
 	} else {
 		console.log("YOUR BROWSER NOT SUPPORT!");
 	}
-	const geom = new THREE.BoxGeometry(80, 80, 80);
-	const mtl = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-	const box = new THREE.Mesh(geom, mtl);
-	const DOC = new THREEx.DeviceOrientationControls(CAMERA);
-	// const LOADER = new GLTFLoader().setPath("./src/models/");
-	// createLoader(
-	// 	LOADER,
-	// 	ARJS,
-	// 	AMBIENT,
-	// 	"bds-tugu-kujang.glb",
-	// 	-6.394179290603103,
-	// 	106.84657327029028
-	// );
-	ARJS.add(box, -6.394179290603103, 106.84657327029028);
+	{
+		const geom = new THREE.BoxGeometry(80, 80, 80);
+		const mtl = new THREE.MeshBasicMaterial({ color: 0xb1e1ff });
+		const box = new THREE.Mesh(geom, mtl);
+		ARJS.add(box, -6.394179290603103, 106.84657327029028);
+	}
+	{
+		const LOADER = new GLTFLoader().setPath("./src/models/");
+		createLoader(
+			LOADER,
+			ARJS,
+			AMBIENT,
+			"bds-tugu-kujang.glb",
+			-6.394179290603103,
+			106.84657327029028
+		);
+	}
 	// Create the device orientation tracker
 	// ARJS.fakeGps(-0.720003747118712, 51.050002962571995);
 	ARJS.startGps();
@@ -42,8 +50,7 @@ function main() {
 			CANVAS.height != CANVAS.clientHeight
 		) {
 			RENDERER.setSize(CANVAS.clientWidth, CANVAS.clientHeight, false);
-			const ASPECT = CANVAS.clientWidth / CANVAS.clientHeight;
-			CAMERA.aspect = ASPECT;
+			CAMERA.aspect = CANVAS.clientWidth / CANVAS.clientHeight;
 			CAMERA.updateProjectionMatrix();
 		}
 		DOC.update();
